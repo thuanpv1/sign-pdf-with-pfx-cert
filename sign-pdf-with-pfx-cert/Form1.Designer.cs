1227b66f580fae5e06e66aaf0ba6861e0b739956
thuanpv1 <thuanpv@egt.vn>
2021-12-02 09:08:27 +0700

    add reference to project

--- sign-pdf-with-pfx-cert/Form1.Designer.cs
+++ sign-pdf-with-pfx-cert/Form1.Designer.cs
@@ -28,13 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxPathToPdf = new System.Windows.Forms.TextBox();
+            this.textBoxPathToPfx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxCoordinateX = new System.Windows.Forms.TextBox();
+            this.textBoxCoordinateY = new System.Windows.Forms.TextBox();
+            this.textBoxPageNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxPfxPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sign";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxPathToPdf
+            // 
+            this.textBoxPathToPdf.Location = new System.Drawing.Point(175, 82);
+            this.textBoxPathToPdf.Name = "textBoxPathToPdf";
+            this.textBoxPathToPdf.Size = new System.Drawing.Size(671, 22);
+            this.textBoxPathToPdf.TabIndex = 1;
+            // 
+            // textBoxPathToPfx
+            // 
+            this.textBoxPathToPfx.Location = new System.Drawing.Point(175, 121);
+            this.textBoxPathToPfx.Name = "textBoxPathToPfx";
+            this.textBoxPathToPfx.Size = new System.Drawing.Size(671, 22);
+            this.textBoxPathToPfx.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path to pdf";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Path to pfx file";
+            // 
+            // textBoxCoordinateX
+            // 
+            this.textBoxCoordinateX.Location = new System.Drawing.Point(175, 198);
+            this.textBoxCoordinateX.Name = "textBoxCoordinateX";
+            this.textBoxCoordinateX.Size = new System.Drawing.Size(205, 22);
+            this.textBoxCoordinateX.TabIndex = 5;
+            // 
+            // textBoxCoordinateY
+            // 
+            this.textBoxCoordinateY.Location = new System.Drawing.Point(175, 237);
+            this.textBoxCoordinateY.Name = "textBoxCoordinateY";
+            this.textBoxCoordinateY.Size = new System.Drawing.Size(204, 22);
+            this.textBoxCoordinateY.TabIndex = 6;
+            // 
+            // textBoxPageNumber
+            // 
+            this.textBoxPageNumber.Location = new System.Drawing.Point(175, 279);
+            this.textBoxPageNumber.Name = "textBoxPageNumber";
+            this.textBoxPageNumber.Size = new System.Drawing.Size(203, 22);
+            this.textBoxPageNumber.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Coordinate X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Coordinate Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 282);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Page number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(62, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Pfx password";
+            // 
+            // textBoxPfxPassword
+            // 
+            this.textBoxPfxPassword.Location = new System.Drawing.Point(175, 160);
+            this.textBoxPfxPassword.Name = "textBoxPfxPassword";
+            this.textBoxPfxPassword.Size = new System.Drawing.Size(205, 22);
+            this.textBoxPfxPassword.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(879, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxPfxPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPageNumber);
+            this.Controls.Add(this.textBoxCoordinateY);
+            this.Controls.Add(this.textBoxCoordinateX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPathToPfx);
+            this.Controls.Add(this.textBoxPathToPdf);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxPathToPdf;
+        private System.Windows.Forms.TextBox textBoxPathToPfx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxCoordinateX;
+        private System.Windows.Forms.TextBox textBoxCoordinateY;
+        private System.Windows.Forms.TextBox textBoxPageNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxPfxPassword;
     }
 }
 
--- sign-pdf-with-pfx-cert/Form1.cs
+++ sign-pdf-with-pfx-cert/Form1.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.security;
+using Org.BouncyCastle.X509;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using X509Certificate = Org.BouncyCastle.X509.X509Certificate;
 
 namespace sign_pdf_with_pfx_cert
 {
@@ -15,6 +21,131 @@ namespace sign_pdf_with_pfx_cert
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public string SignPDF(string pdfPath, string pfxPath, string pfxPassword,  int locationx, int locationy, int numPage)
+        {
+            byte[] bytes = File.ReadAllBytes(pdfPath);
+            string strRe = "";
+            try
+            {
+                //Initialize the Windows store.
+                X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                //store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+                X509Certificate2 certClient = null;
+                certClient = new X509Certificate2(File.ReadAllBytes(pfxPath), pfxPassword, X509KeyStorageFlags.Exportable);
+
+
+                //Get Cert Chain
+                IList<X509Certificate> chain = new List<X509Certificate>();
+                X509Chain x509Chain = new X509Chain();
+
+                x509Chain.Build(certClient);
+
+                foreach (X509ChainElement x509ChainElement in x509Chain.ChainElements)
+                {
+                    chain.Add(Org.BouncyCastle.Security.DotNetUtilities.FromX509Certificate(x509ChainElement.Certificate));
+                }
+
+                string dtn = DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() + DateTime.Now.Second.ToString() + DateTime.Now.Millisecond.ToString();
+                System.IO.FileStream stream = new FileStream(@".\" + dtn + ".pdf", FileMode.CreateNew);
+                System.IO.BinaryWriter writer = new BinaryWriter(stream);
+                writer.Write(bytes, 0, bytes.Length);
+                writer.Close();
+
+                PdfReader inputPdf = new PdfReader(@".\" + dtn + ".pdf");
+
+                FileStream signedPdf = new FileStream(@".\" + dtn + "_signed.pdf", FileMode.Create);
+
+                PdfStamper pdfStamper = PdfStamper.CreateSignature(inputPdf, signedPdf, '\0');
+
+                IExternalSignature externalSignature = new X509Certificate2Signature(certClient, "SHA-1");
+
+                Console.WriteLine(certClient);
+
+                PdfSignatureAppearance signatureAppearance = pdfStamper.SignatureAppearance;
+
+                int positionx = locationx + 5;
+                int positiony = 832 - (locationy + 55);
+                int positionyrel = positiony - 55;
+                int positionxrel = positionx + 160;
+
+                signatureAppearance.SetVisibleSignature(new iTextSharp.text.Rectangle(positionx, positiony, positionxrel, positionyrel), numPage, "Signature");
+                signatureAppearance.Reason = "I have reviewed this document";
+                signatureAppearance.Location = "Hanoi";
+                signatureAppearance.Contact = "Email: info@egt.com.vn";
+                //signatureAppearance.SignatureGraphic = Image.GetInstance(100, 200,, @"D:\\tick.png");
+
+                //signatureAppearance.Image = iTextSharp.text.Image.GetInstance(@"D:\\logosmartsign.png");
+                //signatureAppearance.ImageScale = 0.36f;
+                //signatureAppearance.Image.Alignment = 300;
+                //signatureAppearance.Acro6Layers = true;
+
+
+
+                MakeSignature.SignDetached(signatureAppearance, externalSignature, chain, null, null, null, 0,
+                    CryptoStandard.CMS);
+
+
+
+                inputPdf.Close();
+                pdfStamper.Close();
+
+
+                File.Delete(@".\" + dtn + ".pdf");
+
+                Byte[] input = File.ReadAllBytes(@".\" + dtn + "_signed.pdf");
+
+                strRe = Convert.ToBase64String(input);
+            }
+            catch (Exception) {
+
+            }
+            return strRe;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pathToPdf = this.textBoxPathToPdf.Text;
+            string pathToPfx = this.textBoxPathToPfx.Text;
+            string pfxPassword = this.textBoxPfxPassword.Text;
+            try
+            {
+                double coorX = Double.Parse(this.textBoxCoordinateX.Text);
+                double coorY = Double.Parse(this.textBoxCoordinateY.Text);
+                int pageNum = Int32.Parse(this.textBoxPageNumber.Text);
+
+                // validate path
+                if (!File.Exists(pathToPdf))
+                {
+                    MessageBox.Show("Path to pdf file is not valid", "Error");
+                }
+                if (!File.Exists(pathToPfx))
+                {
+                    MessageBox.Show("Path to pfx file is not valid", "Error");
+                }
+                if (pageNum < 0)
+                {
+                    pageNum = 0;
+                }
+
+                if (coorX < 0) coorX = 0;
+                if (coorY < 0) coorY = 0;
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                MessageBox.Show("Something is wrong coordinateX or coordinateY or pageNumber occurred, please check it again", "Error");
+            }
+
         }
     }
 }
